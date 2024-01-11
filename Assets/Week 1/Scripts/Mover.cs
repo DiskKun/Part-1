fb17293b9de8a9a8fe184429207c64cad794eba8
Lucas Hourahine <luca5h1905@gmail.com>
2024-01-11 11:26:17 -0500

    Task 2b completed: firing a missile

--- Assets/Week 1/Scripts/Mover.cs
+++ Assets/Week 1/Scripts/Mover.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     public float speed = 5;
+    public GameObject missilePrefab;
+    public Transform spawn;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +18,11 @@ public class Mover : MonoBehaviour
     {
         float direction = Input.GetAxis("Horizontal");
         transform.Translate(speed * Time.deltaTime * direction, 0, 0);
+        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(missilePrefab, spawn.position, spawn.rotation);
+        }
     }
 }
